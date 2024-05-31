af2947208341237a270b2547113e6617dcc289ab
HEADLINE Design <aaron@headline.dev>
2024-05-31 11:33:44 -0500

    update auth

--- siws-app/lib/auth.ts
+++ siws-app/lib/auth.ts
@@ -184,7 +184,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: 'lax',
         path: '/',
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? 'siws.org' : 'localhost',
+        domain: VERCEL_DEPLOYMENT ? 'dotstack.xyz' : 'localhost',
         secure: VERCEL_DEPLOYMENT,
       },
     },
